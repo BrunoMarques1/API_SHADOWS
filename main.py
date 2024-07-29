156a7d747a8dc7f3f82565fc9f4c37c2881f2f50
Bruno Marques <brunomarques.1@hotmail.com>
2024-07-28 23:46:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, registry
 from pydantic import BaseModel
-from dotenv import load_dotenv
- 
-load_dotenv()
+import os
 
-engine = create_engine("mysql+pymysql://root:123@mysql:3306/cinema")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:3306/{db_name}"
+
+engine = create_engine(db_url)
 mapper_registy = registry()
 Base = mapper_registy.generate_base()
 Session = sessionmaker(bind=engine)
@@ -29,30 +33,27 @@ class Filmes(Base):
     def __repr__(self):
         return f"Filme [titulo={self.titulo}, ano={self.ano}]"
 
-
-
 @app.get("/")
 async def get():
-    return ("OLÁ MUNDO!")
-
+    return "OLÁ MUNDO!"
 
 @app.get("/get")
 async def get():
     data = session.query(Filmes).all()
-    return (data)
+    return data
 
 @app.post("/")
-async def post(body : Body):
-    data_insert = Filmes(titulo=body.titulo, genero=body.genero, ano= body.ano)
+async def post(body: Body):
+    data_insert = Filmes(titulo=body.titulo, genero=body.genero, ano=body.ano)
     session.add(data_insert)
     session.commit()
 
 @app.delete("/")
-async def delete(body : Body):
-    session.query(Filmes).filter(Filmes.titulo==body.titulo).delete()
-    session.commit()    
+async def delete(body: Body):
+    session.query(Filmes).filter(Filmes.titulo == body.titulo).delete()
+    session.commit()
 
 @app.put("/titulo={titulo}")
-async def put(body: Body,titulo):
-    session.query(Filmes).filter(Filmes.titulo==titulo).update({"titulo":body.titulo,"genero":body.genero,"ano":body.ano})
+async def put(body: Body, titulo):
+    session.query(Filmes).filter(Filmes.titulo == titulo).update({"titulo": body.titulo, "genero": body.genero, "ano": body.ano})
     session.commit()
